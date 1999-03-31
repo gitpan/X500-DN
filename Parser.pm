3205084ea95bb643bd8141b853acfc02a79e128a
Ron Savage <ron@savage.net.au>
1999-03-31 11:02:12 +0000

    Import of RSAVAGE/X500-DN-1.11 from CPAN.
    gitpan-cpan-distribution: X500-DN
    gitpan-cpan-version:      1.11
    gitpan-cpan-path:         RSAVAGE/X500-DN-1.11.tgz
    gitpan-cpan-author:       RSAVAGE
    gitpan-cpan-maturity:     released

--- Parser.pm
+++ Parser.pm
@@ -1,4 +1,4 @@
-package	X500::DN::Parser;
+package X500::DN::Parser;
 
 # File:
 # -------------------------------------------------------------------
@@ -15,20 +15,23 @@ package	X500::DN::Parser;
 # -------------------------------------------------------------------
 
 use strict;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
 
-use vars qw(@EXPORT @EXPORT_OK @ISA);
-use vars qw($VERSION);
+require Exporter;
 
-use Exporter;
+@ISA = qw(Exporter);
 
-@ISA		= qw(Exporter);
+# Items to export into callers namespace by default. Note: do not export
+# names by default without a very good reason. Use EXPORT_OK instead.
+# Do not simply export all your public functions/methods/constants.
+
 @EXPORT		= qw();
+
 @EXPORT_OK	= qw();
 
-# -------------------------------------------------------------------
+$VERSION	= '1.11';
 
-$VERSION    = '1.10';
-
+# Preloaded methods go here.
 # -------------------------------------------------------------------
 
 sub invalidDN
@@ -207,4 +210,35 @@ sub parse
 
 #-------------------------------------------------------------------
 
+# Autoload methods go after =cut, and are processed by the autosplit program.
+
 1;
+__END__
+# Below is the stub of documentation for your module. You better edit it!
+
+=head1 NAME
+
+X500::DN - Perl extension for blah blah blah
+
+=head1 SYNOPSIS
+
+  use X500::DN;
+  blah blah blah
+
+=head1 DESCRIPTION
+
+Stub documentation for X500::DN was created by h2xs. It looks like the
+author of the extension was negligent enough to leave the stub
+unedited.
+
+Blah blah blah.
+
+=head1 AUTHOR
+
+A. U. Thor, a.u.thor@a.galaxy.far.far.away
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
